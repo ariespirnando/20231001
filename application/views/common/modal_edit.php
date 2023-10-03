9a95ecbc04218a2e18c46af7ef6e65ac569c8c73
A Aries Pirnando <ariespirnando@users.noreply.github.com>
2023-10-03 10:24:38 +0700

    Update modal_edit.php

--- application/views/common/modal_edit.php
+++ application/views/common/modal_edit.php
@@ -1,15 +1,14 @@
 <form enctype='multipart/form-data' class="simple-example was-validated" action="<?php echo base_url().'common/edit' ?>" method="post">
 <div class="form-row mb-4"> 
          
-        
+    <input name="type" type="hidden" value="<?php echo $loaddata['type'] ?>">    
 	<?php 
 
 		if($loaddata['type']=="KEPSEK"){
 	
 	?>
     <div class="form-group col-md-8">
-        <label for="inputAddress3">Nama Pegawai</label>  
-		<input name="type" type="hidden" value="<?php echo $loaddata['type'] ?>">
+        <label for="inputAddress3">Nama Pegawai</label>   
 		<input name="nama" type="text" required class="form-control" id="inputState3" placeholder="NAMA" value="<?php echo $loaddata['nama'] ?>">
         <div class="invalid-feedback">
             *Mandatory
@@ -24,8 +23,11 @@
         </div>
     </div>  
 
+	<?php }else{ ?>
+	    <input name="nama" type="hidden" value="<?php echo $loaddata['nama'] ?>">
+	    <input name="nip" type="hidden" value="<?php echo $loaddata['nip'] ?>">
 	<?php } ?>
- 
+    
 	<div class="form-group col-md-12">
 		<label for="inputAddress3">Upload Gambar</label>
 		<input accept="image/png, image/jpeg" required name="uploadfile" type="file" required class="form-control" id="inputState3" placeholder="Upload File"> 
